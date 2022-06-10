6a4f54c65f8a8a4ac19c37eab571ae569ce58274
Nero <resqnetm@gmail.com>
2022-06-10 16:19:19 +0300

    Rename safe_uninit macro to the SafeUninit

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Safe wrapper around `Option<T>`
 # Simple unsage
 
 See `examples` folder for more information
+
+# 1.0.6 Breaking changes
+
+renamed macros `safe_uninit!` to `SafeUninit!` to keep project code style
--- src/macros.rs
+++ src/macros.rs
@@ -1,14 +1,14 @@
 /// Easy way to access to the SafeUninit type
 /// 
-/// safe_uninit![T] = safe_uninit![T, deref_mut]
+/// SafeUninit![T] = SafeUninit![T, deref_mut]
 /// 
-/// safe_uninit![T, deref] - implement only Deref trait, no DerefMut
+/// SafeUninit![T, deref] - implement only Deref trait, no DerefMut
 /// 
-/// safe_uninit![T, deref_mut] - implement Deref + DerefMut traits
+/// SafeUninit![T, deref_mut] - implement Deref + DerefMut traits
 /// 
 /// This makes SafeUninit struct transparent to its underlying value
 #[macro_export]
-macro_rules! safe_uninit {
+macro_rules! SafeUninit {
     ($ty:ty) => {
         $crate::safe_uninit![$ty, deref_mut]
     };
